4688ccb70fce1edbeedc723fc5cd200f8bcd1ec5
yahia-y4 <yalhmwd994@gmail.com>
2025-11-11 18:48:55 +0300

    c7

--- src/component/experiences/Experiences.jsx
+++ src/component/experiences/Experiences.jsx
@@ -1,7 +1,10 @@
 import "./Experiences.css"
 import Language from "../Language/Language"
 export default function Experiences(){
-    const lans = ["C++", "C", "Python","JS", "HTML", "CSS","Dart","PHP","SQL"]
+    const lans = ["C","Dart","PHP","SQL","C++","JS","Python"]
+    const front = ["HTML", "CSS", "JS", "React","Next.js","axios","Bootstrap","Material UI","Tailwind CSS"]
+    const back = ["Node.js", "Express.js", "ASP.NET","Laravel","MySQL","SQLite","sql server","REST API"]
+    const other = ["Git","Postman","VS Code","Android Studio","command line"]
     return(
         <div className="experiences">
             <h2 className="experiences-title">الخبرات</h2>
@@ -14,6 +17,36 @@ export default function Experiences(){
                         ))
                     }
                 </div>
+            </div>
+            <div className="web">
+                <h3 className="web-title">تقنيات الويب</h3>
+                <div className="web-container">
+                   <h3 className="web-title">front end</h3>
+                   <div className="web-content">
+                        {
+                            front.map((lan, index) => (
+                                <Language key={index} lan={lan}/>
+                            ))
+                        }
+                   </div>
+                    <h3 className="web-title">back end</h3>
+                <div className="web-content">
+                    {
+                        back.map((lan, index) => (
+                            <Language key={index} lan={lan}/>
+                        ))
+                    }
+                </div>
+                    <h3 className="web-title">اخرى</h3>
+                <div className="web-content">
+                    {
+                        other.map((lan, index) => (
+                            <Language key={index} lan={lan}/>
+                        ))
+                    }
+                </div>
+                </div>
+               
             </div>
             
         </div>
